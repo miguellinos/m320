37df577f956eb0d91091757c561d9fb5dfec608f
Miguel Rectra <50084743+miguellinos@users.noreply.github.com>
2023-09-03 22:39:40 +0200

    Add files via upload

---
+++ Aufgabe_03/Aufgabe_03/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aufgabe_03;
+
+Calculator calculator = new Calculator();
+
+// Testen der Rechenoperationen
+int result = calculator.Addiere(5, 3);
+Console.WriteLine("Addition: " + result);
+
+result = calculator.Subtrahiere(10, 4);
+Console.WriteLine("Subtraktion: " + result);
+
+result = calculator.Multipliziere(6, 2);
+Console.WriteLine("Multiplikation: " + result);
+
+result = calculator.Dividiere(8, 2);
+Console.WriteLine("Division: " + result);
+
+// letztes Ergebnis
+int lastResult = calculator.GetLastResult();
+Console.WriteLine("Letztes Ergebnis: " + lastResult);
+
+var newresult = new Calculator.PerformOperation(7);
+Console.WriteLine("Ihr Resultat ist:" + result);
