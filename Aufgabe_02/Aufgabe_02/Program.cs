d5e2292d8ea0b20e3984d07a509bbf4b7bcedd60
Miguel Rectra <50084743+miguellinos@users.noreply.github.com>
2023-09-03 23:05:23 +0200

    Add files via upload

---
+++ Aufgabe_02/Aufgabe_02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aufgabe_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(5, 3);
+
+            // Überprüfen Sie die Werte mit dem Debugger oder durch Ausgabe
+            Console.WriteLine($"Area: {rectangle.Area}");
+            Console.WriteLine(rectangle.ToString() );
+
+
+        }
+    }
+}
